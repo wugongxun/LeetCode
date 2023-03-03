a001ad4a8a5cc51dfc04a41923fd16c9948ad76a
wugongxun <2297665453@qq.com>
2023-03-03 11:42:13 +0800

    2023.3.3

---
+++ src/main/java/com/wgx/day26/二进制数转字符串.java
@@ -0,0 +1,23 @@
+package com.wgx.day26;
+
+import java.util.ArrayList;
+
+/**
+ * @author wgx
+ * @since 2023/3/2 10:06
+ */
+public class 二进制数转字符串 {
+    public static void main(String[] args) {
+        System.out.println(printBin(0.1));
+    }
+
+    public static String printBin(double num) {
+        var res = new StringBuilder("0.");
+        while (num != 0 && res.length() <= 32) {
+            int i = (int) (num * 2);
+            num = num * 2 - i;
+            res.append(i);
+        }
+        return res.length() <= 32 ? res.toString() : "ERROR";
+    }
+}
