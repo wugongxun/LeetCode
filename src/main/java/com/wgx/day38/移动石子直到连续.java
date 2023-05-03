8bf0c82bf993eb485ab3f1fa79ab220003f3e356
wugongxun <2297665453@qq.com>
2023-05-03 15:18:33 +0800

    2023.5.3

---
+++ src/main/java/com/wgx/day38/移动石子直到连续.java
@@ -0,0 +1,30 @@
+package com.wgx.day38;
+
+import java.util.Arrays;
+
+/**
+ * @author wgx
+ * @since 2023/4/30 20:35
+ */
+public class 移动石子直到连续 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(numMovesStones(1, 4, 3)));
+    }
+
+    public static int[] numMovesStones(int a, int b, int c) {
+        int x = Math.min(Math.min(a, b), c);
+        int z = Math.max(Math.max(a, b), c);
+        int y = a + b + c - x - z;
+        int min = 0;
+        if (z - y == 1 && y - x == 1) {
+            min = 0;
+        } else if (z - y <= 2 || y - x <= 2) {
+            min = 1;
+        } else {
+            min = 2;
+        }
+        int max = z - x - 2;
+
+        return new int[]{min, max};
+    }
+}
---
+++ src/main/java/com/wgx/day38/通知所有员工所需的时间.java
@@ -0,0 +1,63 @@
+package com.wgx.day38;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author wgx
+ * @since 2023/5/1 20:55
+ */
+public class 通知所有员工所需的时间 {
+    @Test
+    public void test() {
+        System.out.println(numOfMinutes(11, 4, new int[]{5,9,6,10,-1,8,9,1,9,3,4}, new int[]{0,213,0,253,686,170,975,0,261,309,337}));
+    }
+
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        var cache = new int[n];
+        Arrays.fill(cache, -1);
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, dfs(manager, informTime, cache, i));
+        }
+        return res;
+    }
+
+    public int dfs(int[] manager, int[] informTime, int[] cache, int i) {
+        if (manager[i] < 0) {
+            return informTime[i];
+        }
+        if (cache[i] != -1) {
+            return cache[i];
+        }
+        return cache[i] = dfs(manager, informTime, cache, manager[i]) + informTime[i];
+    }
+
+
+//    List<Integer>[] tree;
+//    int[] informTime;
+//
+//    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+//        tree = new List[n];
+//        Arrays.setAll(tree, i -> new ArrayList());
+//        for (int i = 0; i < n; ++i) {
+//            if (manager[i] != -1) {
+//                tree[manager[i]].add(i);
+//            }
+//        }
+//        this.informTime = informTime;
+//        return dfs(headID);
+//    }
+//
+//    public int dfs(int cur) {
+//        if (tree[cur].size() == 0) {
+//            return 0;
+//        }
+//        int max = 0;
+//        for (var next : tree[cur]) {
+//            max = Math.max(dfs(next) + informTime[cur], max);
+//        }
+//        return max;
+//    }
+}
