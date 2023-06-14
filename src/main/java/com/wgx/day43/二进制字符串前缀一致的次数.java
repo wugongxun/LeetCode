35e9424ff76cb1436d4c2156bd63fb23bb9cf0e4
wugongxun <2297665453@qq.com>
2023-06-14 14:40:51 +0800

    2023.6.14

---
+++ src/main/java/com/wgx/day43/二进制字符串前缀一致的次数.java
@@ -0,0 +1,46 @@
+package com.wgx.day43;
+
+/**
+ * @author wgx
+ * @since 2023/6/14 9:58
+ */
+public class 二进制字符串前缀一致的次数 {
+    public static void main(String[] args) {
+        System.out.println(numTimesAllBlue(new int[]{3, 2, 4, 1, 5}));
+    }
+
+
+    public static int numTimesAllBlue(int[] flips) {
+        int n = flips.length;
+        int count = 0, farthest = 0, res = 0;
+        for (int i = 0; i < n; ++i) {
+            farthest = Math.max(farthest, flips[i]);
+            if (farthest == i + 1) {
+                ++res;
+            }
+        }
+        return res;
+    }
+
+//    public static int numTimesAllBlue(int[] flips) {
+//        int n = flips.length;
+//        var bits = new boolean[n];
+//        int res = 0;
+//        for (int i = 0; i < n; ++i) {
+//            int flip = flips[i] - 1;
+//            bits[flip] = true;
+//            var flag = true;
+//            for (int j = 0; j < n; j++) {
+//                if ((j <= i && !bits[j])
+//                        || (j > i && bits[j])) {
+//                    flag = false;
+//                    break;
+//                }
+//            }
+//            if (flag) {
+//                ++res;
+//            }
+//        }
+//        return res;
+//    }
+}
